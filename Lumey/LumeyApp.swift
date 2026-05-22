0127a98fe73e5788c49c7f985997c97f5afc463e
Asteria Moon <asteriasmoons@outlook.com>
2026-05-22 13:51:44 -0500

    Initial Commit

---
+++ Lumey/LumeyApp.swift
@@ -0,0 +1,32 @@
+//
+//  LumeyApp.swift
+//  Lumey
+//
+//  Created by Asteria Moon on 5/22/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct LumeyApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
